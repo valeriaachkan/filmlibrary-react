49fcc185dabf11c2ca95e59ca790635bac88b645
valeriaachkan <91270873+valeriaachkan@users.noreply.github.com>
2023-08-26 12:42:32 +0300

    fix bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../pages/Home'));
@@ -19,7 +19,7 @@ const App = () => {
 						<Route path="cast" element={<Cast />} />
 						<Route path="reviews" element={<Reviews />} />
 					</Route>
-					<Route path="*" element={<div>Not Found</div>} />
+					<Route path="*" element={<Navigate to="/" />} />
 				</Route>
 			</Routes>
 		</div>
